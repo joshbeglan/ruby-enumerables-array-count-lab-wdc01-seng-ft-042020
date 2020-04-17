b991ac8ca0946b6db7fa6239dfc7cce1364c6a43
Josh Beglan <josh_beglan@yahoo.com>
2020-04-17 02:58:59 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,12 +1,13 @@
 def count_strings(array)
+  counter = 0
   i = 0
   while i < array.length
-    if array[i] == ""
-      
-    else
+    if array[i] == array[i].to_s
+      counter += 1
     end
     i += 1
   end
+  counter
 end
 
 def count_empty_strings(array)
